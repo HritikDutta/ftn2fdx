a90d3eee8cbab54d5038e42ff86e4b6e3364f14c
ThreewayDude <dutta.hritik@gmail.com>
2021-04-03 21:38:30 +0530

    Main: Now the input file is checked and a proper output file is made
    The input file must have a .fountain extension. If no output file is
    specified then the name of the fountain file is used with .fdx extension
    
    Also the DEBUG define is commented out now.

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include "converter/filestuff.h"
 #include "converter/fountain.h"
 #include "converter/fdx.h"
+#include "converter/helpers.h"
 
-#define DEBUG
+// #define DEBUG
 
 #ifdef DEBUG
 int main()
 {
+    int argc = 2;
     char* argv[] = {
         "fftest",
         "tests/Brick & Steel.fountain"
@@ -16,15 +18,23 @@ int main()
 #else
 int main(int argc, char* argv[])
 {
+#endif
     if (argc < 1)
         return 0;
-#endif
+
+    if (!is_fountain(argv[1]))
+    {
+        printf("What file is this? %s", argv[1]);
+        return 1;
+    }
+
+    String outfile;
+    if (argc == 2)
+        outfile = convert_extension(argv[1]);
+    else
+        outfile = argv[2];
 
     Parser parser = parser_make(load_file(argv[1]));
     parser_parse(&parser);
-
-    generate_fdx(&parser);
-
-    // parser_free(&parser);
-    // string_free(&content);
+    generate_fdx(&parser, outfile);
 }
