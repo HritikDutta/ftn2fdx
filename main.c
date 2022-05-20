cad7097a0c5253f5971c6cf3b081c7b6daccb2d6
HritikDutta <dutta.hritik@gmail.com>
2022-05-20 10:35:35 +0530

    Main: Added quotes around filename for when extension is wrong

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int argc, char* argv[])
 
     if (!is_fountain(argv[1]))
     {
-        printf("What file is this? %s\n", argv[1]);
+        printf("What file is this? \"%s\"\n", argv[1]);
         return 1;
     }
 
